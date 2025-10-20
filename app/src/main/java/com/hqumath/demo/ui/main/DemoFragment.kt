242c0e56082c89a1eafdc0bb758a97fcb4a23ea4
guoyadi <yadig@qq.com>
2025-10-20 19:18:43 +0800

    切换usb相机

--- app/src/main/java/com/hqumath/demo/ui/main/DemoFragment.kt
+++ app/src/main/java/com/hqumath/demo/ui/main/DemoFragment.kt
@@ -1,35 +1,78 @@
 package com.hqumath.demo.ui.main
 
+import android.hardware.usb.UsbDevice
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hqumath.demo.databinding.FragmentDemoBinding
+import com.hqumath.demo.utils.LogUtil
 import com.jiangdg.ausbc.MultiCameraClient
 import com.jiangdg.ausbc.base.CameraFragment
 import com.jiangdg.ausbc.callback.ICameraStateCallBack
+import com.jiangdg.ausbc.camera.CameraUVC
 import com.jiangdg.ausbc.utils.ToastUtils
 import com.jiangdg.ausbc.widget.AspectRatioTextureView
 import com.jiangdg.ausbc.widget.CaptureMediaView
 import com.jiangdg.ausbc.widget.IAspectRatio
 
 class DemoFragment : CameraFragment(), View.OnClickListener, CaptureMediaView.OnViewClickListener {
-    private lateinit var mViewBinding: FragmentDemoBinding
+    private lateinit var binding: FragmentDemoBinding
 
     override fun getRootView(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): View? {
-        mViewBinding = FragmentDemoBinding.inflate(inflater, container, false)
-        return mViewBinding.root
+        binding = FragmentDemoBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun initView() {
+        super.initView()
+        binding.btnGetCameraList.setOnClickListener {
+            getCurrentCamera()?.let { strategy ->
+                if (strategy is CameraUVC) {
+                    val sb = StringBuilder()
+                    val curDevice = strategy.getUsbDevice()
+                    val curDeviceInfo =
+                        "当前: ,${curDevice.productName},${curDevice.deviceName},${curDevice.productId}"
+                    sb.append(curDeviceInfo).append("\n")
+                    LogUtil.d(curDeviceInfo)
+                    //相机列表
+                    val usbDeviceList: MutableList<UsbDevice>? = getDeviceList()
+                    if (usbDeviceList.isNullOrEmpty()) {
+                        ToastUtils.show("Get usb device failed")
+                        return@let
+                    }
+                    for (index in (0 until usbDeviceList.size)) {
+                        val dev = usbDeviceList[index]
+                        val cameraInfo =
+                            "相机${index},${dev.productName},${dev.deviceName},${dev.productId}"
+                        sb.append(cameraInfo).append("\n")
+                        LogUtil.d(cameraInfo)
+                    }
+                    binding.tvInfo.setText(sb.toString())
+                }
+            }
+        }
+
+        binding.btnSwitch.setOnClickListener {
+            try {
+                val index = Integer.parseInt(binding.edtNum.text.toString())
+                switchCamera(getDeviceList()!![index])
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 
     override fun getCameraView(): IAspectRatio {
         return AspectRatioTextureView(requireContext())
     }
 
     override fun getCameraViewContainer(): ViewGroup? {
-        return mViewBinding.cameraViewContainer
+        return binding.cameraViewContainer
     }
 
     override fun getGravity(): Int = Gravity.CENTER
