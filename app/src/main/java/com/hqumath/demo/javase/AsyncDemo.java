3bda756cb6027a2cabfe62fbb3c14ab48d6dfef1
guoyadi <yadig@qq.com>
2025-10-30 08:43:26 +0800

    判断线程是否执行完成

--- app/src/main/java/com/hqumath/demo/javase/AsyncDemo.java
+++ app/src/main/java/com/hqumath/demo/javase/AsyncDemo.java
@@ -11,18 +11,21 @@ package com.hqumath.demo.javase;
 public class AsyncDemo {
 
     //private Random random = new Random(System.currentTimeMillis());
-    private final Object lock = new Object();
+    private final Object lock = new Object(); //线程锁
+    private boolean isFinish = false; //任务是否执行完成
 
     public static void main(String[] args) {
         System.out.println("发起调用");
         AsyncDemo demo = new AsyncDemo();
         demo.asyncCall();
         System.out.println("主线程等待");
-        synchronized (demo.lock) {
-            try {
-                demo.lock.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        synchronized (demo.lock) { //确保在同一时刻只有一个线程能够访问被同步的代码块
+            if (!demo.isFinish) { //任务未执行完成，需要阻塞线程
+                try {
+                    demo.lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
         System.out.println("主线程结束");
@@ -41,8 +44,9 @@ public class AsyncDemo {
                 e.printStackTrace();
             }
             System.out.println("子线程结束,唤醒主线程");
-            synchronized (lock) {
+            synchronized (lock) {//唤醒线程
                 lock.notifyAll();
+                isFinish = true;
             }
         }).start();
     }
